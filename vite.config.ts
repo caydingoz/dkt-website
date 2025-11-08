3edd5be8906d20ef8335d3403fd6fead6a26f503
caydingoz <caydingoz1@gmail.com>
2025-11-08 16:38:50 +0300

    gh pages added.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: "/dkt-website/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
